a504b03522d386fd57e8f0794b07882d485f9b43
Fernanda Montalvan <fernanda.r.montalvan@gmail.com>
2024-10-08 12:47:20 +0200

    Starting to work on login php page

---
+++ php/login.php
@@ -0,0 +1,98 @@
+<?php 
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" name="Fernanda">
+    <meta name="description" content="Lupe's Rentacar, Renta de coches Lupe.">
+    <meta name="keywords" content="renta de autos, renta de carros, lupe, fernanda, auto, automóvil, renta, rent a car">
+    <title>Lupe's Rentacar</title>
+    <link rel="stylesheet" href="../css/cars.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="shortcut icon" href="../icons/coche-electrico.png" type="image/x-icon">
+</head>
+<body>
+    <header>
+        <section id="header">
+            <div id="logo">
+                <a href="../index.html"><img src="../icons/logo.svg" alt="Lupe's Rentacar"></a>
+                <a href="../index.html"><h1>Rentacar</h1></a>
+            </div>
+        
+            <div id="menu">
+                <nav>
+                    <ul>
+                        <li><a href="#">Coches</a></li>
+                        <li><a href="#">Motos</a></li>
+                        <li><a href="#">Contacto</a></li>
+                    </ul>
+                </nav>
+            </div>
+
+            <div id="login">
+                <div class="search-bar">
+                    <input type="text" placeholder="Buscar...">
+                    <button type="submit">Buscar</button>
+                </div>
+        
+                <div class="login">
+                    <button type="submit">Login</button>
+                </div>
+            </div>
+        </section>
+
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+        <nav>
+            <ul>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Nosotros</a>
+                    <ul>
+                        <li><a href="#">Nuestras oficinas</a></li>
+                        <li><a href="#">Nuestros autos</a></li>
+                        <li><a href="#">Nuestros servicios</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Servicios</a>
+                    <ul>
+                        <li><a href="#">Renta de autos</a></li>
+                        <li><a href="#">Renta de motos</a></li>
+                        <li><a href="#">Renta de campers</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Contacto</a>
+                    <ul>
+                        <li><a href="#">Ubicaciones</a></li>
+                        <li><a href="#">Preguntas frecuentes</a></li>
+                        <li><a href="#">Atención al cliente</a></li>
+                    </ul>
+                    <br>
+                    <div class="social-media">
+                        <a href="#"><img src="../img/twitter_x_new_logo_x_rounded_icon_256078.svg" alt="Facebook"></a>
+                        <a href="#"><img src="../img/facebook_icon-icons.com_59205.svg" alt="Instagram"></a>
+                        <a href="#"><img src="../img/Instagram_icon-icons.com_66804.svg" alt="Twitter"></a>
+                    </div>
+                </li>
+            </ul>
+        </nav>
+
+        <div class="newsletter">
+            <h4>Suscríbete a nuestra newsletter</h4>
+            <p>Todos los derechos reservados &copy; Lupe's Rentacar 2021</p>
+        </div>
+        
+    </footer>
+</body>
+</html>
